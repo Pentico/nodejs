3fda66e40fb0f464e34ad2b0797600894c2702fa
Pentico <morulaneat@gmail.com>
2016-09-11 15:14:07 +0200

    Template literal

---
+++ app2.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Tuane on 2016/09/11.
+ */
+var name = 'John Doe';
+var greet = 'Hello' + name;
+
+// template literal
+var greet2 = `Hello ${ name}`;
+console.log(greet);
+console.log(greet2);
