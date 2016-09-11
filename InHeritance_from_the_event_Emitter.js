c55285f5e753e69acb3368cd81af8fce23aefd6a
Pentico <morulaneat@gmail.com>
2016-09-11 14:29:55 +0200

    Inheritance from the event emitter

---
+++ InHeritance_from_the_event_Emitter.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Tuane on 2016/09/11.
+ */
+
+var eventEmiiter = require('events');
+var util =  require('util');
+
+function greetr() {
+    this.greetting = 'Hello World';
+}
+
+util.inherits(greetr, eventEmiiter);
+
+greetr.prototype.greet = function (data) {
+    console.log(this.greetting + ' ' + data);
+    this.emit('greet',data);
+
+};
+
+var greeter1 = new greetr();
+greeter1.on('greet',function (data) {
+    console.log('Someone greeted! ' + data);
+});
+
+greeter1.greet('Heroes');
