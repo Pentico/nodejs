d48cd609ef28b866651cad237197c00513b6f70e
Pentico <morulaneat@gmail.com>
2016-09-11 15:51:37 +0200

    Classes with ES6 part 2

--- InHeritance_from_the_event_Emitter.js
+++ InHeritance_from_the_event_Emitter.js
@@ -2,21 +2,35 @@
  * Created by Tuane on 2016/09/11.
  */
 
+'use strict';
+
 var eventEmiiter = require('events');
 var util =  require('util');
 
-function greetr() {
+class greetr extends eventEmiiter{
+    constructor(){
+        super();
+        this.greeting = 'Hello World';
+    }
+
+    greet(data){
+        console.log(`${this.greetting} :  ${data}`);
+        this.emit('greet',data);
+    }
+}
+
+/*function greetr() {
     eventEmiiter.call(this);
     this.greetting = 'Hello World';
 }
 
 util.inherits(greetr, eventEmiiter); // greetr gets the methods of eventEmitter i.e Inheriance via prototypes
-
-greetr.prototype.greet = function (data) {
+*/
+/*greetr.prototype.greet = function (data) {
     console.log(this.greetting + ' ' + data);
     this.emit('greet',data);
 
-};
+};*/
 
 var greeter1 = new greetr();
 greeter1.on('greet',function (data) {
